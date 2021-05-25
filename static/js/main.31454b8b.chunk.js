19ad0e12d6ab5bd9380c641a8438787adbde7c2a
Nicolae Ciobanu <cnicolae2000@gmail.com>
2021-05-25 09:13:06 +0300

    Updates

---
+++ static/js/main.31454b8b.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[0],{10:function(n,e,t){"use strict";t.r(e);var c=t(1),i=t.n(c),r=t(3),o=t.n(r),s=(t(8),t(0));var a=function(){return Object(s.jsx)("div",{className:"App",children:"Good morning, Mihai!"})};o.a.render(Object(s.jsx)(i.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))},8:function(n,e,t){}},[[10,1,2]]]);
+//# sourceMappingURL=main.31454b8b.chunk.js.map
