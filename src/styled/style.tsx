cb2c58c04e982711c9ff78c669201c7d09e705d2
Nicolae Ciobanu <cnicolae2000@gmail.com>
2021-05-26 14:51:36 +0300

    :lipstick: Add styles for Controls

--- src/styled/style.tsx
+++ src/styled/style.tsx
@@ -1,10 +1,16 @@
 import styled from "styled-components";
 
-const Container = styled.div`
+const Content = styled.div`
     border: 1px solid black;
     display: grid;
     grid-template-columns: 1fr 1fr;
     max-width: 1200px;
     margin: 0 auto;
 `
-export { Container };
+const Controls = styled.div`
+    background-color: red;
+`
+const Button = styled.button`
+    color: red;
+`
+export { Content, Controls, Button };
