0b31432e28b9d1e25273ecace8c53bf2c2fca019
Nicolae Ciobanu <cnicolae2000@gmail.com>
2021-05-26 19:58:30 +0300

    :zap: + :bug:  Improve Controls performance; Fix overlapping error

--- src/components/controls/index.tsx
+++ src/components/controls/index.tsx
@@ -10,29 +10,31 @@ type ControlProps = {
 
 export const Controls = ({ start, reset, pause, status }: ControlProps) => {
     //Added this array of strings to fix >
-    //This condition will always return 'false' since the types have no overlap
-    //on line 34
-    const statusText = ["Running", "Pause", "Resume", "Finished"];
+    //"This condition will always return 'false' since the types have no overlap" on line 34
+    const statusText= ["Running", "Paused", "Resume", "Finished"];
     return(
         <SC.Controls>
             {!status && (
-                <SC.Button onClick={()=>start}>
+                <SC.ControlsButton onClick={()=>start} className="start">
                     Start Timer
-                </SC.Button>
+                </SC.ControlsButton>
             )}
             {status === statusText[3] && (
-                <SC.Button onClick={()=>start}>
+                <SC.ControlsButton onClick={()=>start} className="start">
                     Restart Timer
-                </SC.Button>
+                </SC.ControlsButton>
             )}
             {(status === statusText[1] || status === statusText[0]) && (
                 <div>
-                    <SC.Button onClick={()=>reset}>
+                    <SC.ControlsButton onClick={()=>reset} className="reset">
                         Reset
-                    </SC.Button>
-                    <SC.Button onClick={()=>pause}>
+                    </SC.ControlsButton>
+                    <SC.ControlsButton
+                        onClick={() => pause}
+                        className={status=== statusText[1]? "resume" : "pause"}
+                    >
                         {status === statusText[1] ? statusText[2] : statusText[1]}
-                    </SC.Button>
+                    </SC.ControlsButton>
                 </div>
             )}
         </SC.Controls>
