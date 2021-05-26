b718d26dc29e102519e08cb6e1dea39555f3f40d
Nicolae Ciobanu <cnicolae2000@gmail.com>
2021-05-26 20:01:43 +0300

    :+1: Display shortcuts

--- src/components/pomodoro/index.tsx
+++ src/components/pomodoro/index.tsx
@@ -1,11 +1,13 @@
 import React, {useState} from "react";
 import { Controls } from "../";
+import { Shortcuts } from "../";
 
 type PomodoroProps={}
 
 export const Pomodoro = ({ }: PomodoroProps) => {
     return (
         <div>
+            <Shortcuts/>
             Pomodoro comes here.
         </div>
     )
