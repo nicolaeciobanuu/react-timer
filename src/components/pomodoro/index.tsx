d22b0b84f9bf54cbdce52dd7bd34bf08869cd763
Nicolae Ciobanu <cnicolae2000@gmail.com>
2021-05-26 14:50:48 +0300

    :recycle: Change code to typescript rules

--- src/components/pomodoro/index.tsx
+++ src/components/pomodoro/index.tsx
@@ -1,14 +1,14 @@
-import { useState } from "react";
+import React, {useState} from "react";
+import { Controls } from "../";
 
-export const Pomodoro = (props) => {
-    //Delcaring state
-    const [selectedType, setSelectedType] = useState(props.types[0]);
-    const [time, setTime] = useState(props.types[0].time);
-    const [interval, setInterval] = useState(null);
-    const [running, setRunning] = useState(false);
+type PomodoroProps={}
 
-    return(
+export const Pomodoro = ({ }: PomodoroProps) => {
+    const [test, useTest] = useState(2);
+    return (
         <div>
-            Pomodoro here
-        </div>)
+            Pomodoro comes here.
+        </div>
+    )
 }
+    
