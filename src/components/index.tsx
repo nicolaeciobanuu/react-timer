c88a0e3048cc8f727a2ca2c8e3cf1126f0a0bb7f
Nicolae Ciobanu <cnicolae2000@gmail.com>
2021-05-26 20:00:59 +0300

    :art: Export all the components from barrel

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,8 @@
 export { Pomodoro } from "./pomodoro";
 export { Controls } from "./controls";
-
-
+export { Shortcuts } from "./shortcuts";
+export { TimeDisplay } from "./time-display";
+export { TypeSelect } from "./type-select";
+export { ToggleSound } from "./toggle-sound";
 
 export { SC } from "../styled";
