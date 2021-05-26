2e4e3eb7809a08df59282a00116f34f0a9994009
Nicolae Ciobanu <cnicolae2000@gmail.com>
2021-05-26 10:20:57 +0300

    :bug: Change allowed type for function

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,7 +1,7 @@
-const addZero = (num: Number) => {
+const addZero = (num: number) => {
     return num > 9 ? num : `0${num}`;
 }
-export const formatTime = (time) => {
+export const formatTime = (time: number) => {
     const minutes = addZero(Math.floor(time / 60));
     const seconds = addZero(Math.floor(time % 60));
 
