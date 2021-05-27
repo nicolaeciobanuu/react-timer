ee2c8a606c444ec4a915b023ab371ed0670d7c80
Nicolae Ciobanu <cnicolae2000@gmail.com>
2021-05-27 16:27:11 +0300

    :fire: Delete volume toggle, export too

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,6 +3,5 @@ export { Controls } from "./controls";
 export { Shortcuts } from "./shortcuts";
 export { TimeDisplay } from "./time-display";
 export { TypeSelect } from "./type-select";
-export { ToggleSound } from "./toggle-sound";
 
 export { SC } from "../styled";
