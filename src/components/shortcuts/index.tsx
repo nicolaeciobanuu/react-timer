2761064c8ec95bd713c3ac34e23333fb648adb39
Nicolae Ciobanu <cnicolae2000@gmail.com>
2021-05-27 22:00:00 +0300

    :fire: Delete Play and Reset shortcut due to not being able to implement

--- src/components/shortcuts/index.tsx
+++ src/components/shortcuts/index.tsx
@@ -3,8 +3,6 @@ import { SC } from "../";
 import { FaKeyboard } from "react-icons/fa"
 
 const shortcutsList: { name: string, key: string }[] = [
-    {"name": "Play / Pause / Resume", "key": "Space"},
-    {"name": "Reset", "key": "Esc"},
     {"name": "Pomodoro", "key": "1"},
     {"name": "Short Break", "key": "2"},
     {"name": "Long Break", "key": "3"}
